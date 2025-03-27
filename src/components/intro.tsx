d9efa01018ae18fdfb07b05943c7171f6ffc1e1f
Matthias <m_a_eschbach@hotmail.com>
2025-03-27 17:30:13 +0100

    update2

--- src/components/intro.tsx
+++ src/components/intro.tsx
@@ -1,6 +1,6 @@
 "use client"
 
-import Youtube1 from "@/components/youtube1";
+//import Youtube1 from "@/components/youtube1";
 import Banner1 from "@/components/banner1";
 
 
@@ -9,10 +9,10 @@ const Intro = () => {
   return (
     <>
       <Banner1 />
-      <div>
+      {/*<div>
         <p className="text-center px-3 pt-20 pb-7 text-m sm:text-3xl text-header font-bold overline">How to use this site? A step-by-step tutorial.</p>
         <Youtube1 />
-      </div>
+      </div>*/}
       <div>
         <p className="px-3 pt-3 text-xl">Disclaimer:</p>
         <p className="px-3 text-sm">The functionality of this site only works on desktop and laptop computers, not on mobile devices. An app is in development. Since a Chrome extension is required, this site only functions completely via the Chrome or Brave browser. The required Chrome extension is a blockchain wallet (e.g. Eternl, Lace or Yoroi). Via this wallet, a plant collection can be recorded on the blockchain. For blockchain transactions, a small amount must be paid (in the order of magnitude of 0.3 Ada, which currently corresponds to approx. $0.20).</p>
